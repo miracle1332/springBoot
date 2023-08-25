7666ae3aff3847454d8f3a709e3da55cb0414171
ohhyerin <lyneen@naver.com>
2023-08-25 15:34:34 +0900

    first commit

---
+++ src/main/java/rin/rinspring/RinSpringApplication.java
@@ -0,0 +1,13 @@
+package rin.rinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rin/rinspring/RinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package rin.rinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
